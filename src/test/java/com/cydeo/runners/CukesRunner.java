063e490c8254336662b257e31a8397752bc04064
BariQapbash <99431864+BariQapbash@users.noreply.github.com>
2022-06-16 14:35:59 +0200

    day 14 continue

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -7,9 +7,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = "",
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
-        dryRun = true
+        dryRun = false,
+        tags = "@employee and not @admin"
 )
 public class CukesRunner {
 }
