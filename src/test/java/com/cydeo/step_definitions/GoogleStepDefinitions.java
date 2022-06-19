70ce444efc1b697b8860e1136eeba7c2b8b6f0d2
BariQapbash <99431864+BariQapbash@users.noreply.github.com>
2022-06-19 22:53:14 +0200

    day 16 continue

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -1,13 +1,33 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.GoogleSearchPage;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.PageFactory;
 
 public class GoogleStepDefinitions {
 
+    GoogleSearchPage googleSearchPage = new GoogleSearchPage();
+
+
+    @When("user types apple and clicks enter")
+    public void user_types_apple_and_clicks_enter() {
+        googleSearchPage.searchBox.sendKeys("apple"+ Keys.ENTER);
+    }
+    @Then("user sees apple in the google title")
+    public void user_sees_apple_in_the_google_title() {
+        String expectedTitle = "apple - Cerca con Google";
+        String actualTitle=Driver.getDriver().getTitle();
+        Assert.assertEquals("Title is not as expected",expectedTitle,actualTitle);
+
+        // Junit assertion accepts first arg as expected, second arg as actual
+        //Assert.assertEquals(expectedTitle,actualTitle);
+        //Assert.assertTrue(actualTile.equals(expectedTitle));
+    }
     @When("user is on Google search page")
     public void user_is_on_google_search_page() {
         Driver.getDriver().get("https://www.google.com");
@@ -15,12 +35,11 @@ public class GoogleStepDefinitions {
     }
     @Then("user should see title is Google")
     public void user_should_see_title_is_google() {
-        String expectedTitle = "Google";
-        String actualTile = Driver.getDriver().getTitle();
 
-        Assert.assertEquals(actualTile,expectedTitle);
-
-        Driver.closeDriver();
     }
+
+
+
+
 
 }
